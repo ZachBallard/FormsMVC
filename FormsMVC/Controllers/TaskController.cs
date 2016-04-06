895fc5c0792649d8834b1fb98b4edbb9b8b57cfa
Zach Ballard <zbginji@gmail.com>
2016-04-06 09:03:58 -0500

    changing create.cshtml to work better

---
+++ FormsMVC/Controllers/TaskController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormsMVC.Controllers
+{
+    public class TaskController : Controller
+    {
+        public List<Task> tasks { get; set; } = new List<Task>();
+
+        // GET: Task
+        [HttpGet]
+        public ActionResult Index(string task)
+        {
+            return View(tasks);
+        }
+        
+        // Create new task
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Task
+        [HttpPost]
+        public ActionResult Create(Task newTask)
+        {
+            tasks.Add(newTask);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FormsMVC/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormsMVC
+{
+    public class Task
+    {
+        [Required]
+        public virtual string User { get; set; }
+        public virtual string TaskDescription { get; set; }
+        public virtual bool IsComplete { get; set; }
+
+    }
+}
